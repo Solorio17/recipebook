ea51d46cb3fbbf1352e41b52ac1a9e2a072f8549
Jason <jasonsoloriom@gmail.com>
2022-07-23 01:36:30 -0600

    This is just a minor change in what the navbar looks like

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,17 +1,13 @@
 import React from "react";
-import CommentDetail from "./CommentDetail";
-import FavoriteRecipes from "./FavoriteRecipes";
 
 const HomePage = (props) => {
     return (
         <div className='ui container'>
-            <div className="ui three item menu">
-                <a className="active item" href='/'>Featured Recipes</a>
+            <div className="ui secondary menu">
+                <a className="item" href='/'>Featured Recipes</a>
                 <a className="item" href='/favorite-recipes'>Favorite Recipes</a>
                 <a className="item" href='/post-recipe'>Post Recipe</a>
             </div>
-
-
             {props.children}
         </div>
     )
